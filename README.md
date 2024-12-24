86aadfaedce11ab9d698c5914e653187bbf96ec9
Sergio Eduardo Weege Junior <sergio.eduardo@havan.com.br>
2024-12-24 20:11:27 -0300

    novo

---
+++ README.md
@@ -0,0 +1,64 @@
+# coding-project-template
+
+## Overview
+This project has 50 points for 20 tasks. As part of the assignment, at least one of your peers will grade your final assignment. Similarly, you will grade one of your peers’ projects. Your peer evaluator will be provided with the link to your GitHub repository and the link to your website. Each peer evaluator will receive objective grading criteria so they can determine how many points to assess for each task.
+
+## Grading Criteria Overview
+### 50 points, 20 tasks
+### GitHub (6 points, 2 tasks)
+1. GitHub repository public URL: **2 points --OK--**
+
+2. Redux-related files and code: 4 points
+
+### Landing page (5 points, 4 tasks)
+1. Background image: **1 point --OK--**
+
+2. Paragraph about the company: **1 point --OK--**
+
+3. Company name: **1 point --OK--**
+
+4. Get Started button linking to the product listing page: **2 points --OK--**
+
+### Product listing page (9 points, 3 tasks)
+1. Six unique houseplants for sale, each displaying thumbnail, name, and price: **2 points --OK--**
+
+2. Group the plants into at least three categories on the page: **1 point --OK--**
+
+3. An Add to Cart button for each plant, each with the following behavior: 6 points
+
+3.1. After selecting it, the shopping cart icon increases by one. **--OK--**
+
+3.2. After selecting it, the button becomes disabled. **--OK--**
+
+3.3. After selecting it, the appropriate plant gets added to the shopping cart. **--OK--**
+
+### Header (7 points, 3 tasks)
+1. Displays on both the product listing page and shopping cart page 2 points
+
+2. A shopping cart icon with a value that displays the total number of items in the cart: 3 points
+
+3. Navigation to either of the other pages: **2 points --OK--**
+
+### Shopping cart page (23 points, 8 tasks)
+1. The total number of plants in the cart: 2 points
+
+2. The total cost of all items in the cart: **2 points --OK--**
+
+3. A checkout button (displays the message “Coming Soon” or similar): **1 point --OK--**
+
+4. A continue shopping button that links to the product listing page: **2 points --OK--**
+
+5. Each plant type in the cart displays a thumbnail, name, and unit price: **6 points --OK--**
+
+6. Increase button for each plant type in the cart that increments the number of items in the cart by one each time it's clicked and updates all appropriate values: **4 points --OK--**
+
+7. Decrease button for each plant type in the cart that decrements the number of items in the cart by one each time it's clicked and updates all appropriate values: **4 points --OK--**
+
+8. A delete button: **2 points --OK--**
+
+===
+
+SETUP
+
+npm install
+npm run dev
---
+++ src/CartItem.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItem, updateQuantity } from './CartSlice';
+import './CartItem.css';
+
+const CartItem = ({ onContinueShopping }) => {
+
+  const cart = useSelector(state => state.cart.items);
+  const dispatch = useDispatch();
+
+  const calculateTotalAmount = () => {
+    return cart.reduce((total, item) => {
+      return total + parseFloat(calculateTotalCost(item));
+    }, 0).toFixed(2);
+  };
+  const calculateQuantityItem = () => {
+    return cart.reduce((totalqtd, item) => {
+      return totalqtd + parseFloat(calculateTotalItens(item));
+    }, 0).toFixed(0);
+  };
+
+  const handleContinueShopping = () => {
+    onContinueShopping();
+  };
+
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+  };
+  const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
+  };
+
+  const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+    totitem = totitem - 1;
+  };
+
+  const calculateTotalCost = (item) => {
+    return (parseFloat(item.cost.replace('$', '')) * item.quantity).toFixed(2);
+  };
+  const calculateTotalItens = (item) => {
+    return (item.quantity).toFixed(0);
+  };
+
+  const handleSubmit = (event) => {
+    alert('Comming soon!');
+  };
+
+  return (
+    <div className="cart-container">
+      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      <h3 style={{ color: 'black' }}>Quantity itens: {calculateQuantityItem()}</h3>
+      <div>
+        {cart.map(item => (
+          <div className="cart-item" key={item.name}>
+            <img className="cart-item-image" src={item.image} alt={item.name} />
+            <div className="cart-item-details">
+              <div className="cart-item-name">{item.name}</div>
+              <div className="cart-item-cost">{item.cost}</div>
+              <div className="cart-item-quantity">
+                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <span className="cart-item-quantity-value">{item.quantity}</span>
+                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+              </div>
+              <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
+              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
+      <div className="continue_shopping_btn">
+        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <br />
+        <button className="get-started-button1" onClick={handleSubmit}>Checkout</button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
